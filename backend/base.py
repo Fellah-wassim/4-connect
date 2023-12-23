d2b20a4cf670f04e878e9181c921be10ead1930b
wassim <wbvb41@gmail.com>
2023-12-24 00:04:30 +0100

    fixed the backend

--- backend/base.py
+++ backend/base.py
@@ -12,41 +12,34 @@ playInstance = Play()
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
+    emit('initial_state', {'board': gameBoard.board})
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
-
-@socketio.on('human_move')
-def handle_human_move(data):
-    column = data['column']
-    playInstance.humanTurn(column, gameBoard)
-    game_over_result = gameBoard.gameOver()
-    emit('update_state', {'board': gameBoard.board, 'game_over': game_over_result}, broadcast=True)
-
-@socketio.on('computer_move')
+@socketio.on('computer1_move')
 def handle_computer_move():
-    playInstance.computerTurn(gameBoard)
-    game_over_result = gameBoard.gameOver()
-    emit('update_state', {'board': gameBoard.board, 'game_over': game_over_result}, broadcast=True)
+    if playInstance.turn == 2 and gameBoard.gameOver() == -1:
+        playInstance.computerTurn(gameBoard)
+        game_over_result = gameBoard.gameOver()
+        emit('update_state', {'board': gameBoard.board, 'game_over': game_over_result}, broadcast=True)
     
-@socketio.on('move')
+@socketio.on('human_move')
 def handle_move(data):
-    player = data['player']
-    column = data['column']
+    if playInstance.turn == 1 and gameBoard.gameOver() == -1:
+        column = data['column']
+        playInstance.humanTurn(column, gameBoard)
+        game_over_result = gameBoard.gameOver()
+        if game_over_result == -1:
+            playInstance.computerTurn(gameBoard)
+            game_over_result = gameBoard.gameOver()
 
-    gameBoard.makeMove(column, gameBoard.getPossibleMoves()[column][0], player)
-
-    # Check if the game is over
-    game_over_result = gameBoard.gameOver()
-
-    # Broadcast the updated state and game over result to all connected clients
-    emit('update_state', {'board': gameBoard.board, 'game_over': game_over_result}, broadcast=True)
+        emit('update_state', {'board': gameBoard.board, 'game_over': game_over_result}, broadcast=True)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # You can create this HTML file later
+    return render_template('index.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
