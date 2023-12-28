ef1407586ba756d983bcf912448cab10fa8b0e80
wassim <wbvb41@gmail.com>
2023-12-28 20:38:11 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      purple2: "#321798",
+      lightPurple: "#7561D0",
+      darkPurple: "#17074E",
+    },
     extend: {},
   },
   plugins: [],
