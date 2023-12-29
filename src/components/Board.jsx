0d7c186146de4889f8d52f3316978be3ad3ebf10
wassim <wbvb41@gmail.com>
2023-12-29 03:59:31 +0100

    updated the board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,38 @@
+/* eslint-disable react/prop-types */
+import yellowCircle from "../assets/game/yellowCircle.png";
+import redCircle from "../assets/game/redCircle.png";
+
 export const Board = (props) => {
+  function renderSwitch(value) {
+    switch (value) {
+      case 1:
+        return <img src={redCircle} alt="red circle" />;
+
+      case 2:
+        return <img src={yellowCircle} alt="yellow circle" />;
+
+      default:
+        // if (props.move[1] === col) {
+        //   return props.turn === 1 ? (
+        //     <img src={yellowCircle} alt="yellow circle" />
+        //   ) : (
+        //     <img src={redCircle} alt="red circle" />
+        //   );
+        // }
+        return (
+          <div className="bg-darkPurple w-16 h-16 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center relative overflow-hidden">
+            <div className="bg-purple w-16 h-16 max-md:w-8 max-md:h-8 rounded-full absolute top-[-10px] max-md:top-[-5px]"></div>
+          </div>
+        );
+    }
+  }
+
   const renderCell = (row, col, value) => (
     <div
       key={col}
-      className={`${
-        value === 1 ? "bg-black" : "bg-white"
-      } w-16 h-16 border border-gray-500 rounded-full flex items-center justify-center`}
+      className="w-16 h-16 max-md:w-8 max-md:h-8 rounded-full flex items-center justify-center m-2"
     >
-      {value}
+      {renderSwitch(value, col)}
     </div>
   );
 
@@ -15,11 +41,16 @@ export const Board = (props) => {
       {rowData.map((value, col) => renderCell(row, col, value))}
     </div>
   );
+
   return (
-    props.board && (
-      <div className="w-fit flex flex-col items-cente">
-        {props.board.map((rowData, row) => renderRow(row, rowData))}
-      </div>
-    )
+    <>
+      {props.board && (
+        <div className="w-fit bg-darkPurple p-2 rounded-2xl">
+          <div className="w-fit flex flex-col items-cente bg-lightPurple rounded-2xl">
+            {props.board.map((rowData, row) => renderRow(row, rowData))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
