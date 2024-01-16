80b5b67216a781f8efb9e08cc0e8c89340531dbe
wassim <wbvb41@gmail.com>
2024-01-16 23:45:40 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,17 @@
-import { useState } from "react";
-import axios from "axios";
-import logo from "/vite.svg";
-import "./App.css";
+import MainMenu from "./pages/MainMenu.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HumanVsBot from "./pages/HumanVsBot.jsx";
+import { BotVsBot } from "./pages/BotVsBot.jsx";
 
 function App() {
-  // new line start
-  const [profileData, setProfileData] = useState(null);
-  axios.defaults.baseURL = "http://127.0.0.1:5000";
-
-  function getData() {
-    axios
-      .get("/profile")
-      .then((response) => {
-        console.log(response);
-        const res = response.data;
-        setProfileData({
-          profile_name: res.name,
-          about_me: res.about,
-        });
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  }
-  //end of new line
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        {/* new line start*/}
-        <p>To get your profile details: </p>
-        <button
-          onClick={() => {
-            getData();
-            console.log(profileData);
-          }}
-        >
-          Click me
-        </button>
-        {profileData && (
-          <div>
-            <p>Profile name: {profileData.profile_name}</p>
-            <p>About me: {profileData.about_me}</p>
-          </div>
-        )}
-        {/* end of new line */}
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/human-vs-bot" element={<HumanVsBot />} />
+        <Route path="/bot1-vs-bot2" element={<BotVsBot />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
